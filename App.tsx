3837f1d70744f32e3a0dab41e71f13acfb85f4be
huulocmedvnu <tthloc.stu13@medvnu.edu.vn>
2020-04-20 12:20:46 +0700

    Convert project to typescript

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Ga nuong muoi ot</Text>
+      <Button
+        title="Xem cach che bien"
+        onPress={() => navigation.navigate('Details1')}
+      />
+      <Text>Thit heo quay</Text>
+        <Button
+          title="Xem cach che bien"
+          onPress={() => navigation.navigate('Details2')}
+      />
+    </View>
+  );
+}
+
+
+function DetailsScreen1({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Thit ga + muoi ot</Text>
+    </View>
+  );
+}
+
+function DetailsScreen2({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Thit heo</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details1" component={DetailsScreen1} />
+        <Stack.Screen name="Details2" component={DetailsScreen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
